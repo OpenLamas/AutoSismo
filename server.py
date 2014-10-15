a9ef9e016266b64322bd3054ccd04b36d8ce96f0
CkuT <s3cu@hotmail.fr>
2014-10-15 16:13:03 +0200

    Fix for double earthquakes

--- server.py
+++ server.py
@@ -18,18 +18,21 @@ def ea(day=None):
     else:
         day = datetime.strptime(day, "%Y-%m-%d")
 
-    for _ in range(2):
+    ea_file = os.path.join(Earthquake.data_folder, 'earthquakes_%s' % day.strftime("%Y-%m-%d"))
+
+    if not os.path.exists(ea_file):
         try:
-            with open(os.path.join(Earthquake.data_folder, 'earthquakes_%s' % day.strftime("%Y-%m-%d"))) as f:
-                earthquake_file_content = f.readlines() or None
-            
-            # Create objects
-            for line in earthquake_file_content[1:]:
-                earthquakes.append(Earthquake(line.split(',')))
-                
-        except IOError:
-            # File not existing, trying to download it.
             Earthquake.get_daily_earthquakes(day.strftime("%Y-%m-%d"))
+        except:
+            pass
+
+    if os.path.exists(ea_file):
+        with open(ea_file) as f:
+            earthquake_file_content = f.readlines() or None
+        
+        # Create objects
+        for line in earthquake_file_content[1:]:
+            earthquakes.append(Earthquake(line.split(',')))
 
     return template('index', earthquakes=earthquakes, day=day.strftime("%A %d %B %Y").capitalize())
 
